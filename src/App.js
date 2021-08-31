905a4a02453756ddb1488f18d41d776159ebd8a2
JelsynCeron <jelsync@gmail.com>
2021-08-31 11:11:23 -0600

    Line new

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Esto es una nueva linea de codigo.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
